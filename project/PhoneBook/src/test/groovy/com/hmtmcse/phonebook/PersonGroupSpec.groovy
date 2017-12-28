071ad129137d2761d32c9d2e2f9b7c03d6fee9b5
Touhid Mia <touhid@bitmascot.com>
2017-12-28 08:23:37 +0600

    PhoneBook

---
+++ project/PhoneBook/src/test/groovy/com/hmtmcse/phonebook/PersonGroupSpec.groovy
@@ -0,0 +1,18 @@
+package com.hmtmcse.phonebook
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PersonGroupSpec extends Specification implements DomainUnitTest<PersonGroup> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
---
+++ project/PhoneBook/src/test/groovy/com/hmtmcse/phonebook/PhoneNumberSpec.groovy
@@ -0,0 +1,18 @@
+package com.hmtmcse.phonebook
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class PhoneNumberSpec extends Specification implements DomainUnitTest<PhoneNumber> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
