bdb24223e583f77068532bb99462afe7633f28d9
Touhid Mia <touhid@bitmascot.com>
2018-08-11 12:55:33 +0600

    Grails Software as a Service Proof of Concept - Official

---
+++ project/GrailsSaaS/GrailsOfficialSaaS/grails-app/controllers/demo/PlanController.groovy
@@ -0,0 +1,27 @@
+package demo
+
+import groovy.transform.CompileStatic
+import groovy.util.logging.Slf4j
+
+@Slf4j
+@CompileStatic
+class PlanController {
+    static allowedMethods = [index: 'GET', save: 'POST', delete: 'DELETE']
+
+    PlanService planService
+
+    def index() {
+        List<Plan> planList = planService.findAll()
+        [planList: planList]
+    }
+
+    def save(SavePlanCommand cmd) {
+        if ( cmd.hasErrors() ) {
+            render status: 422
+            return
+        }
+
+        planService.save(cmd.title)
+        render status: 201
+    }
+}
---
+++ project/GrailsSaaS/GrailsOfficialSaaS/grails-app/services/demo/DatabaseProvisioningService.groovy
@@ -0,0 +1,28 @@
+package demo
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class DatabaseProvisioningService {
+
+    UserRoleService userRoleService
+
+    List<DatabaseConfiguration> findAllDatabaseConfiguration() {
+        List<String> usernames = userRoleService.findAllUsernameByAuthority(VillainService.ROLE_VILLAIN)
+        usernames.collect { findDatabaseConfigurationByUsername(it) }
+    }
+
+    DatabaseConfiguration findDatabaseConfigurationByUsername(String username) {
+        new DatabaseConfiguration(dataSourceName: username, configuration: configurationByUsername(username))
+    }
+
+    Map<String, Object> configurationByUsername(String username) {
+        [
+                'hibernate.hbm2ddl.auto':'none', // <1>
+                'username': 'root', // <2>
+                'password': 'root', // <2>
+                'url':"jdbc:mysql://127.0.0.1:8889/$username" // <2>
+        ] as Map<String, Object>
+    }
+}
+
