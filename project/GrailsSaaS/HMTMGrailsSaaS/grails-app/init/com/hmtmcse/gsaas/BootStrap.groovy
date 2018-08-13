a4e22be3369a6b0ccee0c4743e88bfe813ba7926
Touhid Mia <touhid@bitmascot.com>
2018-08-13 20:08:39 +0600

    Grails Software as a Service Proof of Concept

--- project/GrailsSaaS/HMTMGrailsSaaS/grails-app/init/com/hmtmcse/gsaas/BootStrap.groovy
+++ project/GrailsSaaS/HMTMGrailsSaaS/grails-app/init/com/hmtmcse/gsaas/BootStrap.groovy
@@ -2,16 +2,22 @@ package com.hmtmcse.gsaas
 
 import com.hmtmcse.gsaas.provision.DatabaseProvisioningService
 
+import static grails.gorm.multitenancy.Tenants.withId
+
 
 class BootStrap {
 
     DatabaseProvisioningService databaseProvisioningService
 
     def init = { servletContext ->
-//        databaseProvisioningService.placeHolderDb("hmtm_grails_saas")
+        databaseProvisioningService.placeHolderDb("hmtm_grails_saas")
 
 
-//        AppInitializationService.initialize()
+        withId("hmtm_grails_saas") {
+            AppInitializationService.initialize()
+        }
+
+
     }
 
     def destroy = {
